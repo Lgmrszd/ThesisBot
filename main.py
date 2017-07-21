8e7ed14278f04d8dbece5e3f6fc5d396f7fb195d
Mikhail <m.v.bobrov@yandex.ru>
2017-07-21 18:32:34 +0300

    more features

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import json
 import os.path
+import time
 import telegram
 from telegram import User
 from telegram.ext import Updater, CommandHandler, MessageHandler, typehandler, Filters
 
+towork = True
 
 def c_help(bot, update):
     bot.sendMessage(chat_id=update.message.chat_id,
@@ -11,24 +13,18 @@ def c_help(bot, update):
 /help - Show this""")
 
 
-def main():
-    if os.path.isfile("config.json"):
-        config_file = open("config.json", "r")
-        config = json.load(config_file)
-        TOKEN = config["TOKEN"]
-        config_file.close()
-    else:
-        TOKEN = input("Enter TOKEN: ")
-
-    updater = Updater(TOKEN)
-    # Get the dispatcher to register handlers
-    dp = updater.dispatcher
-    # Add handlers for Telegram messages
-    start_handler = CommandHandler('help', c_help)
-    dp.add_handler(start_handler)
-    updater.start_polling()
-    updater.idle()
-
-
-if __name__ == '__main__':
-    main()
+if os.path.isfile("config.json"):
+    config_file = open("config.json", "r")
+    config = json.load(config_file)
+    TOKEN = config["TOKEN"]
+    config_file.close()
+else:
+    TOKEN = input("Enter TOKEN: ")
+updater = Updater(TOKEN)
+# Get the dispatcher to register handlers
+dp = updater.dispatcher
+# Add handlers for Telegram messages
+help_handler = CommandHandler('help', c_help)
+dp.add_handler(help_handler)
+updater.start_polling()
+updater.idle()
