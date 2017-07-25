e71e8e775c13fe051be66d17bbd6f816169ae280
Mikhail <m.v.bobrov@yandex.ru>
2017-07-25 17:47:35 +0300

    ABSOLUTELY NEW MODE

--- main.py
+++ main.py
@@ -2,21 +2,28 @@ import os.path
 import datetime
 import dbconfig
 import logging
-from telegram import User, InlineQueryResultArticle, InputTextMessageContent, KeyboardButton, ReplyKeyboardMarkup, \
-    InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import Updater, CommandHandler, InlineQueryHandler
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Updater, CommandHandler, CallbackQueryHandler
 
 logging.basicConfig(format='[%(asctime)s][%(levelname)s]:%(message)s', level=logging.DEBUG, datefmt='%d.%m.%Y %H:%M:%S')
 
 
-def thesisToText(interval, text, stime):
+def gen_kb():
+    m30 = InlineKeyboardButton("30 minutes", callback_data="lt:00:30:00")
+    h1 = InlineKeyboardButton("1 hour", callback_data="lt:01:00:00")
+    h5 = InlineKeyboardButton("5 hours", callback_data="lt:05:00:00")
+    h10 = InlineKeyboardButton("10 hours", callback_data="lt:10:00:00")
+    h24 = InlineKeyboardButton("1 day", callback_data="lt:24:59:00")
+    d = InlineKeyboardButton("DELET THIS", callback_data="DELETE")
+    kb = InlineKeyboardMarkup(inline_keyboard=[[m30, h1, h5, h10, h24], [d]])
+    return kb
+
+
+def thesisToText(text, stime):
     return ("\n"
-            "—— ‼️ Theses in last %s ‼️ ——\n"
-            "\n"
             "✅ %s \n"
             "~ published at %s \n"
-            "\n"
-            "—— end of thesis ——") % (interval, text, stime)
+            "\n") % (text, stime)
 
 
 def c_help(bot, update):
@@ -40,7 +47,7 @@ def last30minsTheses(bot, update):
     for t in theses:
         stime = fromUTCtoTZ(t['creation_time']).strftime('%m.%d %H:%M:%S')
         tbody = t['body']
-        st = thesisToText("30 minutes", tbody, stime)
+        st = thesisToText(tbody, stime)
         str_theses.append(st)
     bot.sendMessage(chat_id=message.chat_id,
                     text="Theses in last 30 minutes:\n\n" + "\n".join(str_theses))
@@ -53,10 +60,50 @@ def last5hoursTheses(bot, update):
     for t in theses:
         stime = fromUTCtoTZ(t['creation_time']).strftime('%m.%d %H:%M:%S')
         tbody = t['body']
-        st = thesisToText("5 hours", tbody, stime)
+        st = thesisToText(tbody, stime)
         str_theses.append(st)
     bot.sendMessage(chat_id=message.chat_id,
                     text="Theses in last 5 hours:\n\n" + "\n".join(str_theses))
+
+
+def lastThesesByIntervalToText(chat_id, dt):
+    theses = dbconfig.getLastThesesByTime(chat_id, dt)
+    str_theses = []
+    if theses:
+        for t in theses:
+            stime = fromUTCtoTZ(t['creation_time']).strftime('%m.%d %H:%M:%S')
+            tbody = t['body']
+            st = thesisToText(tbody, stime)
+            str_theses.append(st)
+        str_theses_r = "—— ‼️ Theses in last %s ‼️ ——\n" % dt + "\n".join(str_theses) + "—— end of theses ——"
+        return str_theses_r
+    else:
+        return "No theses available in interval %s" % dt
+
+
+def lastThesesByInterval(bot, update):
+    dt = "00:30:00"
+    message = update.message
+    str_theses = lastThesesByIntervalToText(message.chat_id, dt)
+    bot.sendMessage(chat_id=message.chat_id,
+                    reply_markup=gen_kb(),
+                    text=str_theses)
+
+
+def onCallback(bot, update):
+    print("INTEST")
+    cb = update.callback_query
+    print(cb.data)
+    m_id = cb.message.message_id
+    c_id = cb.message.chat.id
+    print(m_id, c_id)
+    if cb.data == "DELETE":
+        bot.deleteMessage(chat_id=c_id, message_id=m_id)
+    else:
+        dt = cb.data[3:]
+        message = cb.message
+        str_theses = lastThesesByIntervalToText(message.chat_id, dt)
+        bot.editMessageText(chat_id=c_id, message_id=m_id, text=str_theses, reply_markup=gen_kb())
 
 
 def thesis(bot, update, args):
@@ -112,5 +159,9 @@ last30minsTheses_handler = CommandHandler('last30minsTheses', last30minsTheses)
 dp.add_handler(last30minsTheses_handler)
 last5hoursTheses_handler = CommandHandler('last5hoursTheses', last5hoursTheses)
 dp.add_handler(last5hoursTheses_handler)
+lastThesesByInterval_handler = CommandHandler('lt', lastThesesByInterval)
+dp.add_handler(lastThesesByInterval_handler)
+cb_handler = CallbackQueryHandler(onCallback)
+dp.add_handler(cb_handler)
 updater.start_polling()
 updater.idle()
