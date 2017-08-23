7e292714c7279cfc26e7400f82e5af5e70b63b7e
Mikhail <m.v.bobrov@yandex.ru>
2017-08-23 17:39:39 +0300

    edited database creation script

--- dbcreate.py
+++ dbcreate.py
@@ -17,26 +17,68 @@ conn = connect(
 
 cur = conn.cursor()
 
+# cur.execute("""
+# CREATE TABLE users (
+#     user_id         bigint PRIMARY KEY,
+#     username        TEXT,
+#     first_name      TEXT,
+#     last_name       TEXT
+# );
+# """)
+
 cur.execute("""
-CREATE TABLE users (
-    user_id         bigint PRIMARY KEY,
-    username        TEXT,
-    first_name      TEXT,
-    last_name       TEXT
+-- auto-generated definition
+CREATE TABLE users
+(
+  user_id    BIGINT NOT NULL
+    CONSTRAINT users_pkey
+    PRIMARY KEY,
+  username   TEXT,
+  first_name TEXT,
+  last_name  TEXT
 );
 """)
 conn.commit()
 
+# cur.execute("""
+# CREATE TABLE theses (
+#     init_id         bigint,
+#     chat_id         bigint,
+#     user_id         bigint REFERENCES users (user_id),
+#     body            TEXT UNIQUE,
+#     creation_time   timestamp with time zone DEFAULT current_timestamp,
+#     PRIMARY KEY (init_id, chat_id)
+# );
+# """)
+
 cur.execute("""
-CREATE TABLE theses (
-    init_id         bigint,
-    chat_id         bigint,
-    user_id         bigint REFERENCES users (user_id),
-    body            TEXT UNIQUE,
-    creation_time   timestamp with time zone DEFAULT current_timestamp,
-    PRIMARY KEY (init_id, chat_id)
+-- auto-generated definition
+CREATE TABLE theses
+(
+  init_id       BIGINT NOT NULL,
+  chat_id       BIGINT NOT NULL,
+  user_id       BIGINT
+    CONSTRAINT theses_user_id_fkey
+    REFERENCES users,
+  body          TEXT UNIQUE,
+  creation_time TIMESTAMP WITH TIME ZONE DEFAULT current_timestamp,
+  CONSTRAINT theses_pkey
+  PRIMARY KEY (init_id, chat_id)
 );
 """)
+
+conn.commit()
+
+cur.execute("""
+-- auto-generated definition
+CREATE TABLE bot_messages
+(
+  chat_id    BIGINT,
+  message_id BIGINT,
+  owner_id   BIGINT
+);
+""")
+
 conn.commit()
 cur.close()
 conn.close()
